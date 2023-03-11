b479ecc4486862e183c54278e615720be0bd80e7
Tiara-web <80862602+Tiara-web@users.noreply.github.com>
2023-03-10 16:07:41 -0800

    Add files via upload

--- file.js
+++ file.js
@@ -1,12 +1,10 @@
-x = 0;
-y = 0;
+//x = 0;
+//y = 0;
 screen_width = 0;
 screen_height = 0;
 apple = "";
 speak_data = "";
 to_number = "";
-
-draw_apple = "";
 
 draw_apple = "";
 
@@ -33,7 +31,7 @@ recognition.onresult = function (event) {
     draw_apple = "set";
   }
   else {
-    document.getElementById("status").innerHTML = " The speech has not recognized a number";
+    document.getElementById("status").innerHTML = "The speech has not recognized a number";
   }
 
 }
@@ -41,22 +39,27 @@ recognition.onresult = function (event) {
 function setup() {
   screen_width = window.innerWidth;
   screen_height = window.innerHeight;
-  canvas = createCanvas(screen_width, screen_height - 150);
+  canvas = createCanvas(screen_width, (screen_height - 150));
   canvas.position(0, 150);
 
 }
 
 function draw() {
   if (draw_apple == "set") {
+    //document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    //draw_apple = "";
+    //speak_data = to_number + "Apples drawn";
+    //speak();
+    for (var i = 1; i <= to_number; i++) {
+      x = Math.floor(Math.random() * 700);
+      y = Math.floor(Math.random() * 400);
+      image(apple, x, y, 50, 50);
+
+    }
     document.getElementById("status").innerHTML = to_number + " Apples drawn";
     draw_apple = "";
     speak_data = to_number + "Apples drawn";
     speak();
-    for (var i = 1; i <= to_number; i++) {
-      x = Math.floor(Math.random() * 700)
-      y = Math.floor(Math.random() * 400)
-      Image(apple, x, y, 50, 50)
-    }
   }
 }
 
@@ -70,6 +73,6 @@ function speak() {
   speak_data = "";
 }
 
-function preLoad() {
+function preload() {
   apple = loadImage("apple.png");
 }
